3c8c4842b0f6facc3df27a25202c7afd961716ac
altrono <hbotema16@gmail.com>
2023-01-25 23:09:37 +0200

    Added Logics for the Car behaviour

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 
 player = Player()
+car_manager = CarManager()
 
 
 screen.listen()
@@ -19,3 +20,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
