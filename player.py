f3942308fa159b207d63d649931683d095ec8cdc
altrono <hbotema16@gmail.com>
2023-01-25 23:39:03 +0200

    Added Logic to detect when the Player has reached the other side

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player(Turtle):
         super().__init__()
         self.shape('turtle')
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def go_up(self):
@@ -18,3 +18,12 @@ class Player(Turtle):
 
     def go_down(self):
         self.backward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
